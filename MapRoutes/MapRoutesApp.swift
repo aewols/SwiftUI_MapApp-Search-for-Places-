4a11a5267cee624b4feddcde531668355d0a253f
aewols <i0y0j057@gmail.com>
2023-07-12 02:20:29 +0900

    Initial Commit

---
+++ MapRoutes/MapRoutesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapRoutesApp.swift
+//  MapRoutes
+//
+//  Created by 이연정 on 2023/07/12.
+//
+
+import SwiftUI
+
+@main
+struct MapRoutesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
